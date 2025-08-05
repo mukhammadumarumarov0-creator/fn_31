58d03afef110a7f12e47153ad788a8420a6d6f0a
Muhammadumar <mukhammadumarumarov0@gmail.com>
2025-08-05 17:05:47 +0500

    car class yozdik

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ print(users[0])
 def say_hello(name:str):
     return f'Assalom alaykum, {name}'
 print(say_hello('Ali'))
+
+
+class Car():
+    def __init__(self,model:str,brand:str,price:float,color:str):
+        self.model=model
+        self.brand=brand
+        self.price=price
+        self.color=color
+    
+    def to_dict(self):
+        return self.__dict__
+        
